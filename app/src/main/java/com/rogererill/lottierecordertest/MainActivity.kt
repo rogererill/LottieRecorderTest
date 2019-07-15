64bd6714b50872976df02654cf4788992b0f31c0
Roger Erill Carrera <roger.erill@21buttons.com>
2019-07-15 21:21:27 +0200

    Add Lottie to the project

--- app/src/main/java/com/rogererill/lottierecordertest/MainActivity.kt
+++ app/src/main/java/com/rogererill/lottierecordertest/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.rogererill.lottierecordertest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieCompositionFactory
+import com.airbnb.lottie.LottieDrawable
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    val lottieComposition = LottieCompositionFactory.fromRawResSync(this, R.raw.lottielogo)
+    val lottieDrawable = LottieDrawable()
+    lottieDrawable.composition = lottieComposition.value
+  }
 }
